3b6ac7f63b788d58823329882860525a3f1f9c7a
muktanshumishra24 <muktanshumishra24@gmail.com>
2023-10-28 20:55:38 +0530

    V2 API Initialised

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,17 +1,20 @@
 const trackResolvers = require('./trackResolvers');
 const artistResolvers = require('./artistResolvers')
 const albumResolvers = require('./albumResolvers')
+const userResolvers = require('./userResolvers')
 
 module.exports = {
   Query: {
     ...trackResolvers.Query,
     ...artistResolvers.Query,
-    ...albumResolvers.Query
+    ...albumResolvers.Query,
+    ...userResolvers.Query
 
   },
   Mutation: {
     ...trackResolvers.Mutation,
     ...artistResolvers.Mutation,
-    ...albumResolvers.Mutation
+    ...albumResolvers.Mutation,
+    ...userResolvers.Mutation
   },
 };
