6c6d0d90a41d8b0d94c062fd9bf5c25a643dcd2e
Hemang Pant <63491620+hemang-pant@users.noreply.github.com>
2024-01-10 19:05:20 +0530

    bug: removed data leak at addUser

--- graphql/resolvers/userResolvers.js
+++ graphql/resolvers/userResolvers.js
@@ -1,10 +1,13 @@
 const Artist = require("../../models/Artist");
 const User = require("../../models/User");
-const artistResolvers = require('./artistResolvers')
+const artistResolvers = require("./artistResolvers");
 
 module.exports = {
   Mutation: {
-    async addUser(_, { userInput: { name, image, isArtist, address, email, region } }) {
+    async addUser(
+      _,
+      { userInput: { name, image, isArtist, address, email, region } }
+    ) {
       const searchResult = User.find({
         $or: [
           {
@@ -14,68 +17,74 @@ module.exports = {
             },
           },
         ],
-      })
-      // console.log((await searchResult).length)
-      if((await searchResult).length==0){
-      console.log("new user")
-      const addedUser = new User({
-        name: name,
-
-        image: image,
-        isArtist: isArtist,
-        address: address,
-        region: region,
-        email: email
       });
-      const res = await addedUser.save();
-      //Mongo Updated
-      // console.log(res._doc);
-      if(isArtist==true) {
-        artistResolvers.Mutation.addArtist(_,{
-          artistInput: {
-            name: name,
-            description: "description",
-            genre: "Artist Genre"
-          }
-        })
+      // console.log((await searchResult).length)
+      if ((await searchResult).length == 0) {
+        console.log("new user");
+        const addedUser = new User({
+          name: name,
+
+          image: image,
+          isArtist: isArtist,
+          address: address,
+          region: region,
+          email: email,
+        });
+        const res = await addedUser.save();
+        //Mongo Updated
+        // console.log(res._doc);
+        if (isArtist == true) {
+          artistResolvers.Mutation.addArtist(_, {
+            artistInput: {
+              name: name,
+              description: "description",
+              genre: "Artist Genre",
+            },
+          });
+        }
+        //Getting Tracks
+        console.log({
+          id: res.id,
+          ...res._doc,
+        });
+        return {
+          id: res.id,
+          ...res._doc,
+        };
+      } else {
+        console.log("user already exists");
+        const res = (
+          await User.find({
+            $or: [
+              {
+                address: {
+                  $regex: ".*" + address + "",
+                  $options: "i",
+                },
+              },
+            ],
+          })
+        )[0];
+        [
+          res.name,
+          res.email,
+          res.image,
+          // res.isArtist,
+          res.region,
+        ] = "null";
+        res._id = "null";
+        res.id = "null";
+
+        return {
+          id: "null",
+          ...res._doc,
+        };
+        // console.log(searchResult)
+        // return {
+        //   id: searchResult[0].id,
+        //   ...searchResult[0]._doc,
+        // }
       }
-      //Getting Tracks
-      console.log({
-        id: res.id,
-        ...res._doc,
-      })
-      return {
-        id: res.id,
-        ...res._doc,
-      };
-    }
-    else{
-      console.log("user already exists")
-      console.log((await User.find({
-        $or: [
-          {
-            address: {
-              $regex: ".*" + address + "",
-              $options: "i",
-            },
-          },
-        ],
-      }))[0])
-      return (await User.find({
-        $or: [
-          {
-            address: {
-              $regex: address,
-            },
-          },
-        ],
-      })[0])
-      // console.log(searchResult)
-      // return {
-      //   id: searchResult[0].id,
-      //   ...searchResult[0]._doc,
-      // }
-    }
     },
   },
 
